4d5be858b57040bbcc0391e9fa26159e8c691e2d
Diego Sanchez <Diego-24@users.noreply.github.com>
2017-04-04 11:26:09 -0700

    Finally fixed animations!

--- Capstone Game/Assets/Fighter Pack Bundle Free/Code/FighterAnimationDemo.cs
+++ Capstone Game/Assets/Fighter Pack Bundle Free/Code/FighterAnimationDemo.cs
@@ -21,18 +21,26 @@ public class FighterAnimationDemo : MonoBehaviour {
         rotation *= Time.deltaTime;
         transform.Translate(0, 0, translation);
         transform.Rotate(0, rotation, 0);
-        if (Input.GetButtonDown("Punch"))
+        if (Input.GetKeyDown("e"))
         {
-            anim.SetTrigger("Punch");
+            anim.Play("Punch", -1, 0f);
         }
-        if (translation != 0)
+        if (translation > 0)
         {
             anim.SetBool("WalkForward", true);
+            anim.SetBool("WalkBackward", false);
             anim.SetBool("Idle", false);
         }
-        else
+        else if (translation < 0)
         {
             anim.SetBool("WalkForward", false);
+            anim.SetBool("WalkBackward", true);
+            anim.SetBool("Idle", false);
+        }
+        else 
+        {
+            anim.SetBool("WalkForward", false);
+            anim.SetBool("WalkBackward", false);
             anim.SetBool("Idle", true);
         }
     }
